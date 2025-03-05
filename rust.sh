039b59296166475f0384d2e1f4b85588b3e85c41
Brodie Oldfield <Brodie.Oldfield@gmail.com>
2025-03-05 21:59:56 +1100

    Added more

--- rust.sh
+++ rust.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+if ! command -v cargo 2>&1 >/dev/null; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+if command -v cargo 2>&1 >/dev/null; then
+  cargo install eza sshs yazi ripgrep fend dust
+fi
